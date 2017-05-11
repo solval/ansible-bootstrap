fbd8817f99abc25deb939cd4704ddde4c14c6865
Andrzej Maczuga <andrzej.maczuga@intel.com>
2017-05-11 12:19:17 +0000

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ansible-bootstrap
-Prepare ansible launcher host
+Prepare ansible launcher virtualenv directory
+
+# Usage
+The scripts/bootstrap.sh script will:
+- install OS packages, as per requirements file
+- initialize a Python virtualenv directory
+- install Python packages, as per requirements file (including ansible)
+- install Ansible roles, as per requirements file
+
+Default setup can be tweaked using shell environment variables:
+```bash
+$APT_REQ_LIST (defaults to requirements/$ID-$VERSION_ID.txt - as per /etc/os-release)
+$YUM_REQ_LIST (defaults to requirements/$ID-$VERSION_ID.txt - as per /etc/os-release)
+$PIP_REQ_LIST (defaults to requirements/python.txt)
+$ANS_REQ_LIST (defaults to requirements/ansible.yml)
+$VENV_DIR (defaults to $(pwd)/venv)
+```
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Bootstrap ansible launcher virtualenv directory
+
+set -e; set -u
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+else
+    echo "ERROR: can't determine OS distribution, exiting."
+    exit 1
+fi
+
+working_dir=$(pwd)
+aptreqs=${APT_REQ_LIST:-requirements/$ID-$VERSION_ID.txt}
+yumreqs=${YUM_REQ_LIST:-requirements/$ID-$VERSION_ID.txt}
+pipreqs=${PIP_REQ_LIST:-requirements/python.txt}
+ansreqs=${ANS_REQ_LIST:-requirements/ansible.yml}
+venvdir=${VENV_DIR:-$working_dir/venv}
+
+# install required OS packages
+case $ID in
+ubuntu)
+    [ -f "$aptreqs" ] && xargs -a <(awk '/^\s*[^#]/' $aptreqs) -r -- sudo -E apt -y install
+    ;;
+fedora)
+    [ -f "$yumreqs" ] && xargs -a <(awk '/^\s*[^#]/' $yumreqs) -r -- sudo -E yum -y install
+    ;;
+*)
+    echo "ERROR: OS $ID not yet supported, exiting."
+    exit 1
+    ;;
+esac
+
+# set up virtualenv
+virtualenv $venvdir
+set +e; set +u
+source ${venvdir}/bin/activate
+set -e; set -u
+
+# install required python packages
+for pipreq in $pipreqs; do
+  [ -f "$pipreq" ] && pip install -r $pipreq;
+done
+
+# install required ansible roles
+for ansreq in $ansreqs; do
+  [ -f "$ansreqs" ] && ansible-galaxy install -c -p ${working_dir}/roles -r $ansreq
+done
+
+# finalize
+deactivate
